40b32ff92d22671ff225339177f9a3a93d1852c2
Your Name <beeraka.lakshman45@gmail.com>
2023-02-21 21:03:56 +0530

    Add commit

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,6 +1,8 @@
 
 DATE=$(date)
 # date is predefine command in the shell
-echo -e "\e[36m TodayDate is \e0m"
+echo -e "\e[31m TodayDate is \e0m"
 echo "Hello lakshman Today date is :-" ${DATE}
 
+ADD=$((50+45))
+echo "Adding two values is:- " ${ADD}
